15cf54da9850ad72d551d5fe3728d2eec863ba15
raps4f <raps4g@github>
2024-02-26 16:28:03 -0300

    polybar changes - added autorandr profile for double bar

--- polybar/polybar_launcher.sh
+++ polybar/polybar_launcher.sh
@@ -2,7 +2,7 @@
 killall -q polybar
 source $HOME/.config/bspwm/monitors
 #while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-if [ $(autorandr --current) == 'docked' ]; then 
+if [[ $(autorandr --current) == 'docked' || $(autorandr --current) == 'docked_tv' ]]; then 
     MONITOR=$MAIN_MONITOR TRAY_POS=none TRAY_X_OFF=0 polybar main \
         -c $HOME/.config/polybar/config.ini &
     MONITOR=$SECONDARY_MONITOR TRAY_POS=right TRAY_X_OFF=-545 polybar main \
