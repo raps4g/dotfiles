0ce1bad105ff5bfe5243d1e638404ccc4b0d39a3
raps4f <raps4g@github>
2024-02-26 16:28:03 -0300

    new nvim plugins

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -84,6 +84,10 @@ return {
       capabilities = capabilities,
       on_attach = on_attach,
     })
+    lspconfig["lua_ls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
   end,
 }
---
+++ nvim/lua/plugins/mini-surround.lua
@@ -0,0 +1,15 @@
+return {
+  "echasnovski/mini.surround",
+  enabled = true,
+  opts = {
+    mappings = {
+      add = "<leader>sa",
+      delete = "<leader>sd",
+      find = "<leader>sf",
+      find_left = "<leader>sF",
+      highlight = "<leader>sh",
+      replace = "<leader>sr",
+      update_n_lines = "<leader>sn",
+    },
+  },
+}
