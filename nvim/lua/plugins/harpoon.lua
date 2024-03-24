0c41821f02adaea84c792b7ea55a72c029cb0607
raps4g <raps4p@github>
2024-03-24 13:34:19 -0300

    harpoon dvorak keymaps

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -3,11 +3,11 @@ config = function()
     local mark = require("harpoon.mark")
     local ui = require("harpoon.ui")
 
-    vim.keymap.set("n", "<leader>a", mark.add_file)
-    vim.keymap.set("n", "<leader>5", ui.toggle_quick_menu)
-    vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-    vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-    vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+    vim.keymap.set("n", "<A-k>", mark.add_file)
+    vim.keymap.set("n", "<A-i>", ui.toggle_quick_menu)
+    vim.keymap.set("n", "<A-a>", function() ui.nav_file(1) end)
+    vim.keymap.set("n", "<A-o>", function() ui.nav_file(2) end)
+    vim.keymap.set("n", "<A-e>", function() ui.nav_file(3) end)
+    vim.keymap.set("n", "<A-u>", function() ui.nav_file(4) end)
 end,
 }
